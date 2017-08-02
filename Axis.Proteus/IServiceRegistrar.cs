60e4e1397c1a5a08020c99212f02fa16a722fe1c
Stanley Damasus <stanley.damasus@yahoo.com>
2017-08-02 16:52:35 +0100

    added new registration modes

--- Axis.Proteus/IServiceRegistrar.cs
+++ Axis.Proteus/IServiceRegistrar.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Axis.Proteus
 {
@@ -7,9 +8,13 @@ namespace Axis.Proteus
         IServiceRegistrar Register(Type concreteType, object param = null);
         IServiceRegistrar Register(Type serviceType, Type concreteType, object param = null);
         IServiceRegistrar Register(Type serviceType, Func<object> factory, object param = null);
+        IServiceRegistrar Register(Type serviceType, IEnumerable<Type> implementationTypess, object param = null);
+        IServiceRegistrar Register(Type serviceType, IEnumerable<Func<object>> implementationFactories, object param = null);
 
-        IServiceRegistrar Register<Impl>(object param = null);
-        IServiceRegistrar Register<Service>(Func<Service> factory, object param = null);
-        IServiceRegistrar Register<Service, Impl>(object param = null);
+        IServiceRegistrar Register<Impl>(object param = null) where Impl : class;
+        IServiceRegistrar Register<Service>(Func<Service> factory, object param = null) where Service : class;
+        IServiceRegistrar Register<Service, Impl>(object param = null) where Service : class where Impl : class, Service;
+        IServiceRegistrar Register<Service>(IEnumerable<Type> implementationTypes, object param = null) where Service : class;
+        IServiceRegistrar Register<Service>(IEnumerable<Func<Service>> factoryList, object param = null) where Service : class;
     }
 }
