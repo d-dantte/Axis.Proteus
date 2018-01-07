0ab29bc55e923536b3f523f6a1b0ba00f6f16c1f
Stanley Damasus <stanley.damasus@cyberspace.net.ng>
2018-01-07 10:06:39 +0100

    proteus tinz

--- Axis.Proteus/IServiceRegistrar.cs
+++ Axis.Proteus/IServiceRegistrar.cs
@@ -5,16 +5,31 @@ namespace Axis.Proteus
 {
     public interface IServiceRegistrar
     {
-        IServiceRegistrar Register(Type concreteType, object param = null);
-        IServiceRegistrar Register(Type serviceType, Type concreteType, object param = null);
-        IServiceRegistrar Register(Type serviceType, Func<object> factory, object param = null);
-        IServiceRegistrar Register(Type serviceType, IEnumerable<Type> implementationTypess, object param = null);
-        IServiceRegistrar Register(Type serviceType, IEnumerable<Func<object>> implementationFactories, object param = null);
+        IServiceRegistrar Register(Type serviceType, RegistryScope scope = null, InterceptorRegistry registry = null);
+        IServiceRegistrar Register(Type serviceType, Type concreteType, RegistryScope scope = null, InterceptorRegistry registry = null);
+        IServiceRegistrar Register(Type serviceType, Func<object> factory, RegistryScope scope = null, InterceptorRegistry registry = null);
+        IServiceRegistrar Register(Type serviceType, IEnumerable<Type> implementationTypess, RegistryScope scope = null, InterceptorRegistry registry = null);
 
-        IServiceRegistrar Register<Impl>(object param = null) where Impl : class;
-        IServiceRegistrar Register<Service>(Func<Service> factory, object param = null) where Service : class;
-        IServiceRegistrar Register<Service, Impl>(object param = null) where Service : class where Impl : class, Service;
-        IServiceRegistrar Register<Service>(IEnumerable<Type> implementationTypes, object param = null) where Service : class;
-        IServiceRegistrar Register<Service>(IEnumerable<Func<Service>> factoryList, object param = null) where Service : class;
+        IServiceRegistrar Register<Impl>(RegistryScope scope = null, InterceptorRegistry registry = null) where Impl : class;
+        IServiceRegistrar Register<Service>(Func<Service> factory, RegistryScope scope = null, InterceptorRegistry registry = null) where Service : class;
+        IServiceRegistrar Register<Service, Impl>(RegistryScope scope = null, InterceptorRegistry registry = null) where Service : class where Impl : class, Service;
+        IServiceRegistrar Register<Service>(IEnumerable<Type> implementationTypes, RegistryScope scope = null, InterceptorRegistry registry = null) where Service : class;
+    }
+
+    public sealed class RegistryScope
+    {
+        public static readonly RegistryScope Singleton = new RegistryScope("Singleton");
+        public static readonly RegistryScope Transient = new RegistryScope("Transient");
+
+
+        public string Name { get; set; }
+
+        public RegistryScope()
+        { }
+
+        public RegistryScope(string name)
+        {
+            Name = name;
+        }
     }
 }
---
+++ Axis.Proteus/InterceptorRegistry.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Axis.Proteus
+{
+    public class InterceptorRegistry
+    {
+        private HashSet<Type> _proxyIntereptorTypes = new HashSet<Type>();
+
+
+        public InterceptorRegistry(params Type[] types)
+        {
+            types?.ToList().ForEach(RegisterInterceptor);
+        }
+
+        public virtual void RegisterInterceptor(Type type)
+        {
+            _proxyIntereptorTypes.Add(type);
+        }
+
+        public virtual IEnumerable<Type> Interceptors() => _proxyIntereptorTypes.ToArray();
+    }
+}
